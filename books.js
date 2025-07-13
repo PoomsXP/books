ef8eaa3b9881babb848a8aac15949a6ef45d4b84
PoomsXP <115908009+PoomsXP@users.noreply.github.com>
2025-07-12 18:25:07 -0600

    Create books.js

---
+++ books.js
@@ -0,0 +1,8 @@
+// books.js
+const books = [
+  {
+    title: "The Hunger Games",
+    cover: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ2yd8U4zcKPPcVxhhV4YRYqGiAv0rLxAricF-2HpU4P0BRSc2IaXCR4lNxK8AS8V41B_ZnbhrQVN1LOHkA8LiW_Ck0P93s25V82LYWmnQm",
+    pdf: "pdfs/collins-thehungergames.pdf"
+  }
+];
